4f48b40c56eaf6f779c7b9cd86b06ae3286dcde3
D3luX8 <123096715+D3luX8@users.noreply.github.com>
2023-03-14 16:35:04 +0100

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-# Verifica che il numero di argomenti sia corretto
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <directory>"
-  exit 1
-fi
+# Inizializza le variabili per contare il numero di file per ogni interprete shebang
+declare -A counters
 
-# Assegna l'argomento alla variabile
-directory=$1
+# Loop sui file nella directory corrente
+for file in *
+do
+  # Verifica se il file è uno script e contiene uno shebang
+  if [[ -x "$file" && $(head -c 2 "$file") == '#!' ]]
+  then
+    # Estrae l'interprete shebang dal file
+    shebang=$(head -n 1 "$file" | cut -c 3-)
 
-# Verifica che la directory esista e sia una directory
-if [ ! -d "$directory" ]; then
-  echo "Directory not found: $directory"
-  exit 1
-fi
+    # Incrementa il contatore per l'interprete shebang corrispondente
+    counters["$shebang"]=$((counters["$shebang"]+1))
+  fi
+done
 
-# Utilizza il comando 'find' per cercare tutti i file nella directory specificata e nei suoi sottodirectory
-# Il comando 'grep' viene utilizzato per cercare la riga che inizia con '#!' (shebang)
-# Il comando 'cut' viene utilizzato per estrarre l'interprete shebang
-# Il comando 'sort' viene utilizzato per ordinare gli interpreti shebang in ordine alfabetico
-# Il comando 'uniq -c' viene utilizzato per contare il numero di file per ogni interprete shebang
-find "$directory" -type f -exec grep -H '^#!' {} \; | cut -d' ' -f1 --complement | sort | uniq -c
+# Stampa i risultati per ogni interprete shebang
+for shebang in "${!counters[@]}"
+do
+  echo "Interprete shebang '$shebang': ${counters["$shebang"]} file"
+done
 
-# Mostra un messaggio di conferma
-echo "Debug completato con successo per la directory: $directory"
 
